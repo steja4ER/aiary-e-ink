d0f046abb3dc45bbcd0e6a94a8f80575a0d24968
Stefan Janisch <stefan.janisch@4wardenergy.at>
2024-04-07 08:35:30 +0200

    initial commit

--- main.py
+++ main.py
@@ -1,1 +1,37 @@
-import 
+'''Connects to a website and reads the HTML content of the website'''
+
+#!/usr/bin/python
+# -*- coding:utf-8 -*-
+import sys
+import os
+import logging
+import epd2in7_V2
+import time
+from PIL import Image,ImageDraw,ImageFont
+import traceback
+import requests
+
+logging.basicConfig(level=logging.DEBUG)
+
+
+
+
+# Specifay encoding to avoid errors
+encoding = 'utf-8'
+
+url = 'http://aiary.stefanjanisch.net'
+response = requests.get(url)
+html = response.content.decode(encoding)
+
+# Make a list of all lines including "<p><b>"
+lines = html.split('\n')
+lines = [line for line in lines if '<p><b>' in line]
+
+# Split the lines at "</b><br>"
+lines = lines[0].split('</b><br>')
+# Delete <p>, <b>, </p>, </b> and <br> tags
+lines = [line.replace('<p>', '').replace('<b>', '').replace('</p>', '').replace('</b>', '').replace('<br>', '') for line in lines]
+# Delete leading and trailing whitespaces
+lines = [line.strip() for line in lines]
+
+print(lines)
