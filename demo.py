9af55a7097a3a1986bf606e7afe0a24c70eb5c3a
Stefan Janisch <stefan.janisch@4wardenergy.at>
2024-04-08 21:48:43 +0200

    nothing important

--- demo.py
+++ demo.py
@@ -8,7 +8,7 @@ if os.path.exists(libdir):
     sys.path.append(libdir)
 
 import logging
-from waveshare_epd import epd2in13b_V4
+import epd2in13b_V4
 import time
 from PIL import Image,ImageDraw,ImageFont
 import traceback
--- main.py
+++ main.py
@@ -5,7 +5,7 @@
 import sys
 import os
 import logging
-import epd2in13_V4
+import epd2in13b_V4
 import time
 from PIL import Image,ImageDraw,ImageFont
 import traceback
