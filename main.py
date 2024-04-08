85ca1699fb672b0457e2956e23361fdde44ff9a1
Stefan Janisch <stefan.janisch@4wardenergy.at>
2024-04-08 23:07:09 +0200

    new

--- main.py
+++ main.py
@@ -1,32 +1,21 @@
-'''Connects to a website and reads the HTML content of the website'''
-
-#!/usr/bin/python
-# -*- coding:utf-8 -*-
 import sys
 import os
-picdir = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'pic')
-libdir = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'lib')
-if os.path.exists(libdir):
-    sys.path.append(libdir)
-
 import logging
 from waveshare_epd import epd2in13_V4
 import time
-from PIL import Image,ImageDraw,ImageFont
-import traceback
+from PIL import Image, ImageDraw, ImageFont
 import requests
-import time
 
 logging.basicConfig(level=logging.DEBUG)
 
 # Function to automatically split text into multiple lines if it exceeds a certain width
-def split_text_into_lines(text, font, max_width):
+def split_text_into_lines(text, font, max_width, draw):
     words = text.split(' ')
     lines = []
     current_line = ''
     for word in words:
         # Check if adding the next word would exceed the max width
-        test_line = current_line + ' ' + word if current_line else word
+        test_line = f'{current_line} {word}' if current_line else word
         text_width, _ = draw.textsize(test_line, font=font)
         if text_width <= max_width:
             current_line = test_line
@@ -36,9 +25,13 @@ def split_text_into_lines(text, font, max_width):
     lines.append(current_line)  # Add the last line
     return lines
 
-lines = []
+picdir = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'pic')
+libdir = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'lib')
+if os.path.exists(libdir):
+    sys.path.append(libdir)
+
 fut_bold = ImageFont.truetype(os.path.join("Futura Std", 'FuturaStd-Bold.otf'), 18)
-fut_book = ImageFont.truetype(os.path.join("Futura Std", 'FuturaStd-Book.otf'), 18)
+fut_book = ImageFont.truetype(os.path.join("Futura Std", 'FuturaStd-Medium.otf'), 18)
 
 # Specify encoding to avoid errors
 encoding = 'utf-8'
@@ -51,52 +44,30 @@ epd.init()
 epd.Clear(0xFF)
 
 while True:
-    lines_old = lines
-
     response = requests.get(url)
     html = response.content.decode(encoding)
 
-    # Make a list of all lines including "<p><b>"
+    # Extract lines including "<p><b>" and process them
     lines = html.split('\n')
     lines = [line for line in lines if '<p><b>' in line]
-
-    # Split the lines at "</b><br>"
     lines = lines[0].split('</b><br>')
-    # Delete <p>, <b>, </p>, </b> and <br> tags
-    lines = [line.replace('<p>', '').replace('<b>', '').replace('</p>', '').replace('</b>', '').replace('<br>', '') for line in lines]
-    # Delete leading and trailing whitespaces
-    lines = [line.strip() for line in lines]
-    #Replace leading and trailing "
-    lines = [line.replace('"', '') for line in lines]
+    lines = [line.replace('<p>', '').replace('<b>', '').replace('</p>', '').replace('</b>', '').replace('<br>', '').strip().replace('"', '') for line in lines]
 
+    image = Image.new('1', (epd.height, epd.width), 255)
+    draw = ImageDraw.Draw(image)
+    max_width = epd.width - 20
+    y = 10
 
+    # Iterate over lines to apply different fonts and processing
+    for i, line in enumerate(lines):
+        font = fut_bold if i == 0 else fut_book
+        processed_lines = split_text_into_lines(line, font, max_width, draw)
 
-    if lines != lines_old:
-        epd.Clear(0xFF)
-        image = Image.new('1', (epd.height, epd.width), 255)
-        draw = ImageDraw.Draw(image)
+        for pline in processed_lines:
+            text_width, text_height = draw.textsize(pline, font=font)
+            x = (epd.width - text_width) // 2
+            draw.text((x, y), pline, font=font, fill=0)
+            y += text_height + 5  # Adjust spacing between lines if necessary
 
-        max_width = epd.width - 20  # Set max width for text, adjust padding as needed
-
-        # Pre-calculate text height for centering calculation
-        sample_text_width, text_height = draw.textsize("Sample", font=fut_bold)
-
-        processed_lines = []
-        for line in lines:
-            processed_lines.extend(split_text_into_lines(line, fut_bold, max_width))
-
-        # Calculate starting y position to vertically center the text
-        total_height = len(processed_lines) * (text_height + 5) - 5  # Adjust line spacing if necessary
-        y = 10
-
-        # Draw the lines
-        for i, line in enumerate(processed_lines):
-            text_width, _ = draw.textsize(line, font=fut_bold)
-            x = (epd.width - text_width) // 2 + epd.width // 2
-            draw.text((x, y + i * (text_height + 5)), line, font=fut_bold, fill=0)  # Adjust line spacing if necessary
-
-        epd.display(epd.getbuffer(image))
+    epd.display(epd.getbuffer(image))
     time.sleep(60)
-
-    
-
